251b5b407c01aca032e5be90e0c3996d84a17f3d
denisraiko <raikod96@gmail.com>
2024-08-15 12:17:57 +0300

    1.1 Project added

---
+++ MobileUPIntership/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MobileUPIntership
+//
+//  Created by Denis Raiko on 15.08.24.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
