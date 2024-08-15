042111c4cd90894195afd795308a4a7d43011706
denisraiko <raikod96@gmail.com>
2024-08-15 12:22:04 +0300

    1.2 login page added

---
+++ MobileUPIntership/ViewModel/AuthViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  MobileUPIntership
+//
+//  Created by Denis Raiko on 15.08.24.
+//
+
+import UIKit
+import WebKit
+
+class AuthViewController: UIViewController, WKNavigationDelegate {
+    private var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupWebView()
+        loadAuthPage()
+    }
+
+    private func setupWebView() {
+        webView = WKWebView(frame: .zero)
+        webView.navigationDelegate = self
+        
+        view.addSubview(webView)
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 200),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+
+    private func loadAuthPage() {
+        var urlComponent = URLComponents()
+        urlComponent.scheme = "https"
+        urlComponent.host = "oauth.vk.com"
+        urlComponent.path = "/authorize"
+        urlComponent.queryItems = [
+            URLQueryItem(name: "client_id", value: "52149670"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "scope", value: "photos,video"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.131")
+        ]
+        if let authURL = urlComponent.url {
+            let request = URLRequest(url: authURL)
+            webView.load(request)
+        } else {
+            print("Неверный URL")
+        }
+    }
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let url = navigationAction.request.url, url.absoluteString.contains("#access_token=") {
+            if let fragment = url.fragment {
+                let parameters = fragment.split(separator: "&")
+                for param in parameters {
+                    let pair = param.split(separator: "=")
+                    if pair.first == "access_token", let token = pair.last {
+                        UserDefaults.standard.set(String(token), forKey: "vk_access_token")
+                        navigateToMainScreen()
+                        decisionHandler(.cancel)
+                        return
+                    }
+                }
+            }
+        }
+        decisionHandler(.allow)
+    }
+
+    private func navigateToMainScreen() {
+    }
+}
+
